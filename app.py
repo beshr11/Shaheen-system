857643afacab6f6bba16263696704c63d34be9dc
beshr11 <bisho171819@gmail.com>
2025-05-24 08:21:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -72,4 +72,7 @@ def download_previous_projects_pdf():
     return send_file('previous_projects_report_clean.pdf', as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
